e5e39e7a927ba1b368df373f3e9055db087d6291
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-02-25 19:42:36 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// main.js
+import { app, BrowserWindow } from "electron";
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadURL("http://localhost:5173/");
+}
+
+app.whenReady().then(createWindow);
